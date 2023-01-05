2b34640fd322d9e1d14d5e91351ad384babcf60c
John Mickey <john.mickey@trility.io>
2023-01-05 13:04:45 -0600

    Add VPC Flow logs and TFSec remediation

--- clientvpn.tf
+++ clientvpn.tf
@@ -12,40 +12,46 @@ resource "aws_security_group" "vpn_access" {
   name        = "clientvpn"
 
   ingress {
+    description = "https udp from internet"
     from_port   = 443
     protocol    = "UDP"
     to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
   }
   ingress {
+    description = "https tcp from internet"
     from_port   = 443
     protocol    = "tcp"
     to_port     = 443
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
   }
   ingress {
+    description = "dns udp from internet"
     from_port   = 53
     protocol    = "UDP"
     to_port     = 53
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
   }
   ingress {
+    description = "dhs tcp from internet"
     from_port   = 53
     protocol    = "tcp"
     to_port     = 53
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
   }
   ingress {
+    description = "http tcp from internet"
     from_port   = 80
     protocol    = "tcp"
     to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
   }
   egress {
+    description = "all outbount"
     from_port   = 0
     protocol    = "-1"
     to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-egress-sgr
   }
 }
 
@@ -104,13 +110,11 @@ resource "aws_ec2_client_vpn_endpoint" "vpn" {
 resource "aws_ec2_client_vpn_network_association" "subnet_01" {
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn.id
   subnet_id              = aws_subnet.public_subnets["usw2-az1-public"].id
-  #  security_groups     = [aws_security_group.vpn_access.id]
 }
 
 resource "aws_ec2_client_vpn_network_association" "subnet_02" {
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn.id
   subnet_id              = aws_subnet.public_subnets["usw2-az2-public"].id
-  # security_groups     = [aws_security_group.vpn_access.id]
 }
 
 resource "aws_ec2_client_vpn_authorization_rule" "vpn_auth_rule" {
--- vpc.tf
+++ vpc.tf
@@ -6,6 +6,61 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+resource "aws_cloudwatch_log_group" "flow_logs" {
+  name = "${aws_vpc.vpc.id}-flow-logs"
+}
+
+resource "aws_iam_role" "flow_logs" {
+  name = "${aws_vpc.vpc.id}-flow-logs"
+  
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "flow_logs" {
+  name = "${aws_vpc.vpc.id}-flow-logs"
+  role = aws_iam_role.flow_logs.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams",
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_flow_log" "flow_log" {
+  iam_role_arn = aws_iam_role.flow_logs.arn
+  log_destination = aws_cloudwatch_log_group.flow_logs.arn
+  traffic_type = "ALL"
+  vpc_id = aws_vpc.vpc.id
+}
+
 output "vpc_id" {
   value = aws_vpc.vpc.id
 }
